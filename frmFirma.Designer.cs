e5cc374b4906c9764415524c9beba43731dc5022
Nahuel <BKP@DESKTOP-OAA37L8>
2024-05-13 17:57:12 -0300

    Agregar archivos de proyecto.

---
+++ frmFirma.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace pryPachecoEtapa1
+{
+    partial class frmFirma
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pctDibujo = new System.Windows.Forms.PictureBox();
+            this.btnDibujar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pctDibujo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctDibujo
+            // 
+            this.pctDibujo.Location = new System.Drawing.Point(24, 25);
+            this.pctDibujo.Name = "pctDibujo";
+            this.pctDibujo.Size = new System.Drawing.Size(394, 275);
+            this.pctDibujo.TabIndex = 0;
+            this.pctDibujo.TabStop = false;
+            this.pctDibujo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pctDibujo_MouseDown);
+            this.pctDibujo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pctDibujo_MouseMove);
+            this.pctDibujo.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pctDibujo_MouseUp);
+            // 
+            // btnDibujar
+            // 
+            this.btnDibujar.Location = new System.Drawing.Point(24, 329);
+            this.btnDibujar.Name = "btnDibujar";
+            this.btnDibujar.Size = new System.Drawing.Size(93, 40);
+            this.btnDibujar.TabIndex = 1;
+            this.btnDibujar.Text = "Dibujar";
+            this.btnDibujar.UseVisualStyleBackColor = true;
+            this.btnDibujar.Click += new System.EventHandler(this.btnDibujar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(325, 329);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(93, 40);
+            this.btnGuardar.TabIndex = 2;
+            this.btnGuardar.Text = "Guardar Firma";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // frmFirma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(461, 407);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnDibujar);
+            this.Controls.Add(this.pctDibujo);
+            this.Name = "frmFirma";
+            this.Text = "Firmas";
+            this.Load += new System.EventHandler(this.frmFirma_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctDibujo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctDibujo;
+        private System.Windows.Forms.Button btnDibujar;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
+
---
+++ frmFirma.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace pryPachecoEtapa1
+{
+    public partial class frmFirma : Form
+    {
+        public Bitmap Dibujo;
+        public frmFirma()
+        {
+            InitializeComponent();
+            Dibujo = new Bitmap(pctDibujo.Width, pctDibujo.Height);
+            pctDibujo.Image = Dibujo;
+        }
+        bool Clickeando;    //sirve para saber si estamos clickeando, devuelve true o false
+        Point CoordenadasMouse; //las coordenadas del mouse que nos sirven para dibujar
+
+        private void frmFirma_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDibujar_Click(object sender, EventArgs e)
+        {
+            //Activamos/desactivamos botones
+            btnDibujar.Enabled = false;
+            btnGuardar.Enabled = true;
+        }
+
+        private void pctDibujo_MouseDown(object sender, MouseEventArgs e)
+        {
+            Clickeando = true;
+            CoordenadasMouse = e.Location; //Avisamos que estamos clickeando y damos coordenadas
+        }
+
+        private void pctDibujo_MouseUp(object sender, MouseEventArgs e)
+        {
+            Clickeando = false; //Avisamos que no esta clickeando el usuario
+        }
+
+        private void pctDibujo_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Clickeando == true) //Si clickeamos
+            {
+                using (Graphics g = pctDibujo.CreateGraphics()) //Creamos donde dibujar
+                {
+                    using (Pen Lapiz = new Pen(Color.Green, 3)) //Definimos nuestro lapiz
+                    {
+                        g.DrawLine(Lapiz, CoordenadasMouse, e.Location); //Dibujamos linea teniendo en cuenta el lapiz y coordenadas del mouse
+                        CoordenadasMouse = e.Location;  //Sin esto no se movería el lapiz correctamente
+
+                    }
+                }
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string ruta = Path.Combine(Application.StartupPath, "Firmas"); //Donde se inicia la app + la carpeta
+                if (!Directory.Exists(ruta)) //si no existe se crea
+                {
+                    Directory.CreateDirectory(ruta);
+                }
+                string archivo = $"{DateTime.Now.ToString("yyyy-MM-dd hhmmss")}.png"; //nombre del archivo es el dia y hora
+                string destino = Path.Combine(ruta, archivo); //Combinamos ruta y nomb del archivo
+                Dibujo.Save(destino, System.Drawing.Imaging.ImageFormat.Png); //Se guarda
+                MessageBox.Show("¡Firma guardada con exito!");
+                pctDibujo.Invalidate();  //limpiamos dibujo                  
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+
+        }
+    }
+}    
+
